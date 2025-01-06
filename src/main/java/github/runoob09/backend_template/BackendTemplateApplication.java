8d7c85a7f851113e41ffb8f79225ad42fa6600e1
runoob09 <2499469495@qq.com>
2025-01-06 18:31:11 +0800

    init: 项目初始化

---
+++ src/main/java/github/runoob09/backend_template/BackendTemplateApplication.java
@@ -0,0 +1,13 @@
+package github.runoob09.backend_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/runoob09/backend_template/BackendTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package github.runoob09.backend_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
